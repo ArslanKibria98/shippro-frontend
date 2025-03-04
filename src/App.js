9d6597f898fd74b1720461677030b0900adf2cfc
unknown <sarimkwlkwl@gmail.com>
2025-03-04 12:55:18 +0500

    Deploying new versions 1 frontend by shah

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { AuthProvider as AdminauthProvider } from "./context/Adminauth"; // ✅ For admins
 import { useContext } from "react";
@@ -14,35 +14,35 @@ import CreateLabel from "./components/createlabel";
 import BulkUpload from "./components/Bulkupload";
 import LabelsHistory from "./components/labelsHistory";
 import DisplayHistory from "./components/displayHistory";
-
-// Private Route Component for Protected Pages
 const PrivateRoute = ({ element }) => {
     const { user, loading } = useContext(AuthContext);
-
+  
     if (loading) return <p>Loading...</p>;
     return user ? element : <Navigate to="/login" />;
-};
-
+  };
 function App() {
+    
     return (
         <AuthProvider>
-            <AdminauthProvider> {/* ✅ Admin authentication context */}
+                <AdminauthProvider> {/* ✅ Admin authentication context */}
 
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/signup" element={<Signup />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/admin/login" element={<Adminlogin />} />
-                        <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
-                        <Route path="/create-label" element={<CreateLabel />} />
-                        <Route path="/create/bulk" element={<BulkUpload />} />
-                        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                        <Route path="/download-history" element={<DisplayHistory />} />
-                    </Routes>
-                </Router>
+            <Router>
+                <Routes>
+                   <Route path="/" element={<Home />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/admin/login" element={<Adminlogin />} />
+                    <Route path="/dashboard" element={<PrivateRoute element={<Dashboard />} />} />
+                    <Route path="/create-label" element={<CreateLabel />} />
+                    <Route path="/create/bulk" element={<BulkUpload />} />
+                    <Route path="/admin/dashboard" element={<AdminDashboard/>} />
+                    <Route path="/download-history" element={<DisplayHistory/>} />
 
+                    
+                </Routes>
+            </Router>
             </AdminauthProvider> {/* ✅ Admin authentication context */}
+
         </AuthProvider>
     );
 }
